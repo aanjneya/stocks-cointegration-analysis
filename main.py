c690e583d15d099ebda7904cff942b482bbf3ecd
Aanjneya Moudgil <a.moudgil@tum.de>
2025-12-01 02:22:51 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from statsmodels.tsa.stattools import coint
+from find_pairs import findPairs
 from get_data import fetchStocks
+from ols import strategy
 
 pd.set_option('display.max_columns', None)
 
@@ -11,39 +12,8 @@ file = "stocks_sp500.csv"
 loader = fetchStocks(file)
 dfTickers = loader.getAllTickers()
 df = loader.loadDataFromCSV()
+pairs = findPairs(df, dfTickers).getPairs()
 
-sectored_map = {}
-for index, row in dfTickers.iterrows():
-    ticker = row['Symbol']
-    sector = row['GICS Sector']
-    subsector = row['GICS Sub-Industry']
-
-    if sector not in sectored_map:
-        sectored_map[sector] = {}
-
-    if subsector not in sectored_map[sector]:
-        sectored_map[sector][subsector] = []
-
-    sectored_map[sector][subsector].append(ticker)
-
-pairs = []
-
-for i in sectored_map.values():
-    for substocks in i.values():
-        if len(substocks) > 1:
-            for j in range(len(substocks)):
-                if substocks[j] not in df.columns:
-                    continue
-                stock1 = df[substocks[j]]
-                for k in range(j+1, len(substocks)):
-                    if substocks[k] not in df.columns:
-                        continue
-                    stock2 = df[substocks[k]]
-                    merge = pd.concat([stock1,stock2], axis=1)
-                    merge = merge.dropna()
-                    if len(merge)>100:
-                        pval = coint(merge.iloc[:, 0], merge.iloc[:, 1])[1]
-                        if pval < 0.05:
-                            pairs.append({'Stock_a':substocks[j], 'Stock_b':substocks[k], 'p-value':pval})
-
-print(pairs)
+plt  = strategy(df, pairs[1]['stockA'], pairs[1]['stockB'])
+plt.OLS()
+plt.plotOLS()
